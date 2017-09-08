a49d39d3770086afb9431104f62c737eb01efa63
peter <peterjameshogan@gmail.com>
2017-09-08 18:51:52 +0100

    now working with textract to extract pdf text

--- text_reader.py
+++ text_reader.py
@@ -1,22 +1,18 @@
 #!/usr/local/bin/python3.6
 import textract
 import os
+from contextlib import suppress as contextsuppress
 from flask import Flask, make_response, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from random import randrange,random
+
 UPLOAD_FOLDER = '/data'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-'''
-class NumberRequest():
-    def __init__(schema, error_messages):
-        self.schema = schema
-        self.errors = error_messages
-
-    def defaults(**kwargs):
-'''
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 errors = {'missing_var':'Please ensure all required variables are specified.',
           'bad_values':'Please ensure all values are correct.',
@@ -26,13 +22,56 @@ errors = {'missing_var':'Please ensure all required variables are specified.',
 def not_found(error):
     return make_response(jsonify({'error': 'Not Found'}), 404)
 
+@app.route('/upload', methods=['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return make_response(jsonify({'error': 'No file found'}), 400)
+        f = request.files['file']
+        if f.filename == '':
+            return make_response(jsonify({'error': 'Empty POST found.'}), 400)
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            msg = 'File uploaded to {}'.format(str(UPLOAD_FOLDER)+'/'+str(filename))
+            print(msg)
+            return make_response(jsonify({'status': msg}), 200)
+    return make_response(jsonify({'error': 'Please POST a file to process.'}), 405)
+
 @app.route('/extract', methods=['GET','POST'])
 def extract_text():
     if request.method == 'POST':
-        data = request.data
-        files = request.files
-        return repr(files)
+        if 'file' not in request.files:
+            return make_response(jsonify({'error': 'No file found'}), 400)
+        f = request.files['file']
+        if f.filename == '':
+            return make_response(jsonify({'error': 'Empty POST found.'}), 400)
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            filepath = str(UPLOAD_FOLDER)+'/'+str(filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file_text = textract.process(filepath).decode('UTF-8')
+            with contextsuppress(FileNotFoundError):
+                os.remove(filepath)
+            return make_response(jsonify({'file_name': filename, 'file_path': filepath,
+                    'text': file_text}), 200)
+    return make_response(jsonify({'error': 'Please POST a file to process.'}), 405)
 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 @app.route('/random', methods=['GET'])
 def rand_num():
     # Default variables
@@ -90,6 +129,4 @@ def randint():
         ret_tuple.append(num)
     ret_tuple = tuple(ret_tuple)
     return make_response(jsonify({'value': ret_tuple}), 200)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+'''
