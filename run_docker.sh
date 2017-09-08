57f2dd2f2e533e83c1590b686c751daa74e3f404
peter <peterjameshogan@gmail.com>
2017-09-08 18:51:31 +0100

    dynamic document

--- run_docker.sh
+++ run_docker.sh
@@ -2,7 +2,7 @@ sudo docker run --rm \
 		-it  \
 		--name pydock\
 		--hostname pydock\
-		-p 8080:5000\
+		-p 80:5000\
 		-v /home/pine/fluent-python:/usr/src/app \
-		gamma:latest \
+		textract:latest \
 		bash
